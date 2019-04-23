57cfe42e7e4240673d47ce926e370abbd7827e3d
dysir <1030302324@qq.com>
2019-04-23 10:26:51 +0800

    更新缺表和没有权限问题

--- public/install/super.sql
+++ public/install/super.sql
@@ -13,6 +13,7 @@ File Encoding         : 65001
 Date: 2017-11-28 16:15:50
 */
 
+SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `oplog`
 -- ----------------------------
@@ -27,11 +28,127 @@ CREATE TABLE `oplog` (
   `ctime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `ux_user_1` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='日志表';
+) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8 COMMENT='日志表';
 
 -- ----------------------------
 -- Records of oplog
 -- ----------------------------
+INSERT INTO oplog VALUES ('14', 'admin', 'sss', 'ddd', 'sss', 'aa', '2017-10-11 17:17:26');
+INSERT INTO oplog VALUES ('15', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:01:55');
+INSERT INTO oplog VALUES ('16', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:05:29');
+INSERT INTO oplog VALUES ('17', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:06:04');
+INSERT INTO oplog VALUES ('18', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-19 17:06:11');
+INSERT INTO oplog VALUES ('19', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:06:21');
+INSERT INTO oplog VALUES ('20', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:06:57');
+INSERT INTO oplog VALUES ('21', 'admin', '/index.php/m/log', '访问日志', '{\"r\":[\"test\"],\"r', '0.0.0.0', '2017-10-19 17:13:28');
+INSERT INTO oplog VALUES ('22', 'admin', '/index.php/m/log', '访问日志', '{\"r\":[\"test\"],\"r', '0.0.0.0', '2017-10-19 17:15:08');
+INSERT INTO oplog VALUES ('23', 'admin', '/index.php/m/log', '访问日志', '{\"r\":[\"test\"],\"r1\":[\"test2\"]}', '0.0.0.0', '2017-10-19 17:16:09');
+INSERT INTO oplog VALUES ('24', 'admin', '/index.php/m/log', '访问日志', '{\"r\":[\"test\"],\"r1\":[\"test2\"]}', '0.0.0.0', '2017-10-19 17:17:03');
+INSERT INTO oplog VALUES ('25', 'admin', '/index.php/m/log', '访问日志', '{\"r\":[\"test\"],\"r1\":[\"test2\"]}', '0.0.0.0', '2017-10-19 17:17:43');
+INSERT INTO oplog VALUES ('26', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:28:40');
+INSERT INTO oplog VALUES ('27', 'admin', '/index.php/m/log', '访问日志', '', '0.0.0.0', '2017-10-19 17:28:48');
+INSERT INTO oplog VALUES ('28', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-19 18:00:38');
+INSERT INTO oplog VALUES ('29', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-10-20 09:14:30');
+INSERT INTO oplog VALUES ('30', 'admin', '/test/Goods/ajax', 'Array添加商品:1212', '', '192.168.1.115', '2017-10-30 16:37:33');
+INSERT INTO oplog VALUES ('31', 'admin', '/m/user/ajaxAddU', 'Array添加了用户:c2', '', '192.168.1.115', '2017-10-30 19:24:52');
+INSERT INTO oplog VALUES ('32', 'admin', '/m/user/ajaxAddU', 'Array添加了用户:c2', '', '192.168.1.115', '2017-10-30 19:25:01');
+INSERT INTO oplog VALUES ('33', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:c1', '', '192.168.1.115', '2017-10-30 19:29:20');
+INSERT INTO oplog VALUES ('34', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:c11', '', '192.168.1.115', '2017-10-30 19:29:27');
+INSERT INTO oplog VALUES ('35', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-31 09:58:57');
+INSERT INTO oplog VALUES ('36', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:普通怪哪里有', '', '192.168.1.115', '2017-10-31 10:32:27');
+INSERT INTO oplog VALUES ('37', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test1', '', '192.168.1.115', '2017-10-31 11:40:10');
+INSERT INTO oplog VALUES ('38', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test1', '', '192.168.1.115', '2017-10-31 11:40:10');
+INSERT INTO oplog VALUES ('39', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test2', '', '192.168.1.115', '2017-10-31 11:40:37');
+INSERT INTO oplog VALUES ('40', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test3', '', '192.168.1.115', '2017-10-31 11:41:18');
+INSERT INTO oplog VALUES ('41', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test4', '', '192.168.1.115', '2017-10-31 11:41:47');
+INSERT INTO oplog VALUES ('42', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test5', '', '192.168.1.115', '2017-10-31 11:42:12');
+INSERT INTO oplog VALUES ('43', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test6', '', '192.168.1.115', '2017-10-31 11:42:29');
+INSERT INTO oplog VALUES ('44', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test5', '', '192.168.1.115', '2017-10-31 14:05:19');
+INSERT INTO oplog VALUES ('45', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test5', '', '192.168.1.115', '2017-10-31 14:05:20');
+INSERT INTO oplog VALUES ('46', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:g;u', '', '192.168.1.115', '2017-10-31 14:07:50');
+INSERT INTO oplog VALUES ('47', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test6', '', '192.168.1.115', '2017-10-31 14:08:38');
+INSERT INTO oplog VALUES ('48', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-31 14:31:48');
+INSERT INTO oplog VALUES ('49', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-31 15:18:18');
+INSERT INTO oplog VALUES ('50', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-10-31 15:18:18');
+INSERT INTO oplog VALUES ('51', 'test1', '/m/auth/login', 'test1登录了', '', '192.168.1.115', '2017-10-31 15:18:23');
+INSERT INTO oplog VALUES ('52', 'test1', '/m/auth/login', 'test1登录了', '', '192.168.1.115', '2017-10-31 15:18:31');
+INSERT INTO oplog VALUES ('53', 'test1', '/m/auth/login', 'test1登录了', '', '192.168.1.115', '2017-10-31 15:20:14');
+INSERT INTO oplog VALUES ('54', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-10-31 15:20:58');
+INSERT INTO oplog VALUES ('55', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-10-31 15:23:27');
+INSERT INTO oplog VALUES ('56', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-10-31 15:29:54');
+INSERT INTO oplog VALUES ('57', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test5', '', '192.168.1.115', '2017-10-31 15:57:28');
+INSERT INTO oplog VALUES ('58', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test6', '', '192.168.1.115', '2017-10-31 15:57:46');
+INSERT INTO oplog VALUES ('59', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-10-31 16:08:29');
+INSERT INTO oplog VALUES ('60', 'admin', '/m/log', '访问日志', '', '192.168.1.115', '2017-11-03 16:48:37');
+INSERT INTO oplog VALUES ('61', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-03 16:49:05');
+INSERT INTO oplog VALUES ('62', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-03 16:55:24');
+INSERT INTO oplog VALUES ('63', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-03 16:55:25');
+INSERT INTO oplog VALUES ('64', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-03 17:18:15');
+INSERT INTO oplog VALUES ('65', 'test6', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-04 10:23:09');
+INSERT INTO oplog VALUES ('66', 'admin', '/m/auth/login', 'admin登录了', '', '0.0.0.0', '2017-11-06 13:32:43');
+INSERT INTO oplog VALUES ('67', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:34:05');
+INSERT INTO oplog VALUES ('68', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:40:45');
+INSERT INTO oplog VALUES ('69', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:41:32');
+INSERT INTO oplog VALUES ('70', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:41:36');
+INSERT INTO oplog VALUES ('71', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:41:54');
+INSERT INTO oplog VALUES ('72', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:42:03');
+INSERT INTO oplog VALUES ('73', 'admin', '/m/user/ajaxAddU', 'admin添加了用户:test11', '', '0.0.0.0', '2017-11-06 13:44:09');
+INSERT INTO oplog VALUES ('74', 'admin', '/m/User/ajaxAdmi', 'admin修改的密码', '', '0.0.0.0', '2017-11-06 13:44:54');
+INSERT INTO oplog VALUES ('75', 'admin', '/m/User/ajaxAdmi', 'admin修改的密码', '', '0.0.0.0', '2017-11-06 13:45:08');
+INSERT INTO oplog VALUES ('76', 'admin', '/m/User/ajaxAdmi', 'admin修改test11的密码', '', '0.0.0.0', '2017-11-06 13:47:07');
+INSERT INTO oplog VALUES ('77', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:49:29');
+INSERT INTO oplog VALUES ('78', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:57:44');
+INSERT INTO oplog VALUES ('79', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 13:59:19');
+INSERT INTO oplog VALUES ('80', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:06:01');
+INSERT INTO oplog VALUES ('81', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:06:49');
+INSERT INTO oplog VALUES ('82', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:11:23');
+INSERT INTO oplog VALUES ('83', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:11:52');
+INSERT INTO oplog VALUES ('84', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:13:26');
+INSERT INTO oplog VALUES ('85', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:13:40');
+INSERT INTO oplog VALUES ('86', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 14:28:51');
+INSERT INTO oplog VALUES ('87', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:18:28');
+INSERT INTO oplog VALUES ('88', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:19:42');
+INSERT INTO oplog VALUES ('89', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:22:20');
+INSERT INTO oplog VALUES ('90', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:22:48');
+INSERT INTO oplog VALUES ('91', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:23:38');
+INSERT INTO oplog VALUES ('92', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:24:13');
+INSERT INTO oplog VALUES ('93', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:24:47');
+INSERT INTO oplog VALUES ('94', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:25:25');
+INSERT INTO oplog VALUES ('95', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:26:33');
+INSERT INTO oplog VALUES ('96', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:29:49');
+INSERT INTO oplog VALUES ('97', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:39:02');
+INSERT INTO oplog VALUES ('98', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:39:29');
+INSERT INTO oplog VALUES ('99', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:40:02');
+INSERT INTO oplog VALUES ('100', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:40:43');
+INSERT INTO oplog VALUES ('101', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:40:45');
+INSERT INTO oplog VALUES ('102', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:40:54');
+INSERT INTO oplog VALUES ('103', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:40:57');
+INSERT INTO oplog VALUES ('104', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:41:21');
+INSERT INTO oplog VALUES ('105', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:41:23');
+INSERT INTO oplog VALUES ('106', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:41:38');
+INSERT INTO oplog VALUES ('107', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:41:43');
+INSERT INTO oplog VALUES ('108', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 15:42:07');
+INSERT INTO oplog VALUES ('109', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:42:09');
+INSERT INTO oplog VALUES ('110', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:42:31');
+INSERT INTO oplog VALUES ('111', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:43:25');
+INSERT INTO oplog VALUES ('112', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:43:44');
+INSERT INTO oplog VALUES ('113', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:43:54');
+INSERT INTO oplog VALUES ('114', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 15:44:04');
+INSERT INTO oplog VALUES ('115', 'admin', '/m/log/index?cur', '访问日志', '', '0.0.0.0', '2017-11-06 15:44:08');
+INSERT INTO oplog VALUES ('116', 'admin', '/m/log/index?cur', '访问日志', '', '0.0.0.0', '2017-11-06 15:44:13');
+INSERT INTO oplog VALUES ('117', 'admin', '/m/log/index?cur', '访问日志', '', '0.0.0.0', '2017-11-06 15:44:15');
+INSERT INTO oplog VALUES ('118', 'admin', '/m/log/index?cur', '访问日志', '', '0.0.0.0', '2017-11-06 16:55:03');
+INSERT INTO oplog VALUES ('119', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 16:55:09');
+INSERT INTO oplog VALUES ('120', 'admin', '/m/log/index', '访问日志', '', '0.0.0.0', '2017-11-06 17:41:29');
+INSERT INTO oplog VALUES ('121', 'admin', '/m/log/index?st=', '访问日志', '', '0.0.0.0', '2017-11-06 17:41:32');
+INSERT INTO oplog VALUES ('122', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-11-08 11:31:22');
+INSERT INTO oplog VALUES ('123', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-11 14:48:43');
+INSERT INTO oplog VALUES ('124', 'admin', '/m/log/index', '访问日志', '', '192.168.1.115', '2017-11-11 14:48:45');
+INSERT INTO oplog VALUES ('125', 'superadmin', '/m/auth/login', 'superadmin登录了', '', '192.168.1.115', '2017-11-11 17:05:08');
+INSERT INTO oplog VALUES ('126', 'superadmin', '/m/auth/login', 'superadmin登录了', '', '192.168.1.115', '2017-11-11 17:11:19');
+INSERT INTO oplog VALUES ('127', 'superadmin', '/m/user/ajaxAddU', 'superadmin添加了用户:admin', '', '192.168.1.115', '2017-11-11 17:16:10');
+INSERT INTO oplog VALUES ('128', 'admin', '/m/auth/login', 'admin登录了', '', '192.168.1.115', '2017-11-11 17:16:18');
+
 -- ----------------------------
 -- Table structure for `plat_config`
 -- ----------------------------
@@ -207,7 +324,7 @@ INSERT INTO user VALUES ('27', 'test2', '5d1498da42dae6dfe428daee830c8ba80d91c5f
 INSERT INTO user VALUES ('28', 'test3', '16a238671824cfee83294650ab77c7fbcbac48a9', '商品管理_001', 'ov4g653tnvqwqodk', '8', '1', '/test/goods/index,addgoods,editgoods,pullgoods', '2', 'i8a01ufs29ilh389', '2017-10-31 11:41:17', '2017-10-31 11:41:17');
 INSERT INTO user VALUES ('29', 'test4', '841d2b08e6bc766aa21e802cb03ed172b950e597', '店长_001', 'nvtg2zzvhfygom3s', '', '1', '/test/goods/index,addgoods,editgoods,deletegoods,pullgoods,/test/order/index,log', '2', 'ojpbh95sqzcp28zi', '2017-10-31 11:41:46', '2017-10-31 11:41:46');
 INSERT INTO user VALUES ('35', 'test5', '6707b7c4718a624a090ac0658afefa37768cf625', '普通管理员', 'm5rwoylcpayha4lz', '8,7', '2', '/test/goods/index,addgoods,editgoods,pullgoods,/test/order/index,/m/user/index', '2', 'q914qg5t82vd3qz9', '2017-10-31 15:57:28', '2017-10-31 15:57:28');
-INSERT INTO user VALUES ('38', 'admin', 'a9a960043a8992663726170966fa049a85e43d37', '管理员', 'mjxxcmtxo55gqmrx', '', '4', null, '2', '91coxs08968eui35', '2017-11-11 17:16:10', '2017-11-11 17:16:10');
+INSERT INTO user VALUES ('38', 'admin', 'a9a960043a8992663726170966fa049a85e43d37', '管理员', 'mjxxcmtxo55gqmrx', '', '8', null, '2', '91coxs08968eui35', '2017-11-11 17:16:10', '2017-11-11 17:16:10');
 
 -- ----------------------------
 -- Table structure for `user_group`
@@ -259,3 +376,15 @@ INSERT INTO user_group_right VALUES ('32', '9', '66', null, null);
 INSERT INTO user_group_right VALUES ('33', '9', '67', null, null);
 INSERT INTO user_group_right VALUES ('34', '9', '63', null, null);
 INSERT INTO user_group_right VALUES ('35', '9', '60', null, null);
+
+DROP TABLE IF EXISTS `create_log`;
+CREATE TABLE `create_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `authname` varchar(255) DEFAULT NULL COMMENT '推荐权限名',
+  `dirname` varchar(255) DEFAULT NULL COMMENT '推荐目录名',
+  `table` varchar(255) DEFAULT NULL COMMENT '多个表名逗号隔开',
+  `file` text COMMENT '多个文件名逗号隔开',
+  `ctime` datetime DEFAULT NULL,
+  `status` tinyint(2) DEFAULT '1' COMMENT '状态1正常  9 移除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
